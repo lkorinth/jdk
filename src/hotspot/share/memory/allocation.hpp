f4cf550d83ffe599011c69dc3ed2e175700bd86f
Leo Korinth <leo.korinth@oracle.com>
2021-08-12 15:19:15 +0200

    Use thread local to channel allocation type to ResourceObj

--- src/hotspot/share/memory/allocation.hpp
+++ src/hotspot/share/memory/allocation.hpp
@@ -396,16 +396,15 @@ extern void resource_free_bytes( char *old, size_t size );
 class ResourceObj ALLOCATION_SUPER_CLASS_SPEC {
  public:
   enum allocation_type { STACK_OR_EMBEDDED = 0, RESOURCE_AREA, C_HEAP, ARENA, allocation_mask = 0x3 };
-  static void set_allocation_type(address res, allocation_type type) NOT_DEBUG_RETURN;
 #ifdef ASSERT
  private:
   // When this object is allocated on stack the new() operator is not
   // called but garbage on stack may look like a valid allocation_type.
   // Store negated 'this' pointer when new() is called to distinguish cases.
   // Use second array's element for verification value to distinguish garbage.
-  uintptr_t _allocation_t[2];
-  bool is_type_set() const;
-  void initialize_allocation_info();
+  allocation_type _type;
+  static THREAD_LOCAL allocation_type _thread_last_allocated;
+
  public:
   allocation_type get_allocation_type() const;
   bool allocated_on_stack()    const { return get_allocation_type() == STACK_OR_EMBEDDED; }
@@ -413,10 +412,9 @@ class ResourceObj ALLOCATION_SUPER_CLASS_SPEC {
   bool allocated_on_C_heap()   const { return get_allocation_type() == C_HEAP; }
   bool allocated_on_arena()    const { return get_allocation_type() == ARENA; }
 protected:
-  ResourceObj(); // default constructor
-  ResourceObj(const ResourceObj& r); // default copy constructor
-  ResourceObj& operator=(const ResourceObj& r); // default copy assignment
-  ~ResourceObj();
+  ResourceObj();
+  ResourceObj(const ResourceObj&);
+  ResourceObj& operator=(const ResourceObj& r);
 #endif // ASSERT
 
  public:
@@ -431,13 +429,13 @@ protected:
 
   void* operator new(size_t size) throw() {
       address res = (address)resource_allocate_bytes(size);
-      DEBUG_ONLY(set_allocation_type(res, RESOURCE_AREA);)
+      DEBUG_ONLY(_thread_last_allocated = RESOURCE_AREA;)
       return res;
   }
 
   void* operator new(size_t size, const std::nothrow_t& nothrow_constant) throw() {
       address res = (address)resource_allocate_bytes(size, AllocFailStrategy::RETURN_NULL);
-      DEBUG_ONLY(if (res != NULL) set_allocation_type(res, RESOURCE_AREA);)
+      DEBUG_ONLY(if (res != NULL) _thread_last_allocated = RESOURCE_AREA;)
       return res;
   }
 
