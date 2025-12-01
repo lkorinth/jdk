6246240e738c7dbc92f62b80470aa90d23e70f3d
Leo Korinth <leo.korinth@oracle.com>
2025-12-01 19:58:49 +0100

    stefanfix

--- test/hotspot/jtreg/gc/stress/gcold/TestGCOldWithG1.java
+++ test/hotspot/jtreg/gc/stress/gcold/TestGCOldWithG1.java
@@ -32,7 +32,7 @@ package gc.stress.gcold;
  * @summary Stress the G1 GC by trying to make old objects more likely to be garbage than young objects.
  * @run main/othervm -Xmx384M -XX:+UseG1GC gc.stress.gcold.TestGCOldWithG1 50 1 20 10 10000
  * @run main/othervm -Xms64m -Xmx128m -XX:+UseG1GC -XX:+UseDynamicNumberOfGCThreads -Xlog:gc,gc+task=trace gc.stress.gcold.TestGCOldWithG1 50 5 20 1 5000
- * @run main/othervm -Xms64m -Xmx128m -XX:+UseG1GC -XX:+UseDynamicNumberOfGCThreads  -XX:+UnlockDiagnosticVMOptions -Xlog:gc,gc+task=trace gc.stress.gcold.TestGCOldWithG1 50 5 20 1 5000
+ * @run main/othervm -Xms64m -Xmx128m -XX:+UseG1GC -XX:+UseDynamicNumberOfGCThreads  -XX:+UnlockDiagnosticVMOptions -XX:+InjectGCWorkerCreationFailure -Xlog:gc,gc+task=trace gc.stress.gcold.TestGCOldWithG1 50 5 20 1 5000
  */
 public class TestGCOldWithG1 {
     public static void main(String[] args) {
